de78f6533a32e9737faa8d99ed7b8e53e68b905e
pokon548 <65808665+pokon548@users.noreply.github.com>
2023-08-05 20:04:51 +0800

    Small adjustment for electron devshell

--- shell/default.nix
+++ shell/default.nix
@@ -1,7 +1,8 @@
 # Shell for bootstrapping flake-enabled nix and home-manager
 # You can enter it through 'nix develop' or (legacy) 'nix-shell'
 
-{ pkgs }: {
+{ pkgs }:
+{
   default = pkgs.mkShell {
     # Enable experimental features without having to specify the argument
     NIX_CONFIG = "experimental-features = nix-command flakes";
@@ -11,8 +12,14 @@
   # For developing electron apps
   # Wine is included only for cross-compiling Windows binary. Feel free to remove them if you don't need :)
   electron = pkgs.mkShell {
-    nativeBuildInputs = pkgs:
-      (with pkgs; [ nodejs electron wine wine64 stdenv.cc.cc.lib ]);
+    nativeBuildInputs = with pkgs; [
+      yarn
+      nodejs
+      electron
+      wine
+      wine64
+      stdenv.cc.cc.lib
+    ];
 
     LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib64:$LD_LIBRARY_PATH";
     ELECTRON_OVERRIDE_DIST_PATH = "${pkgs.electron}/bin/";
