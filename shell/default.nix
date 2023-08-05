12361eeb24fb6ff5e60c8bdde041ff90e7bc4f02
Bu Kun <65808665+pokon548@users.noreply.github.com>
2023-08-05 18:18:12 +0800

    Add electron devshell

--- shell/default.nix
+++ shell/default.nix
@@ -7,4 +7,14 @@
     NIX_CONFIG = "experimental-features = nix-command flakes";
     nativeBuildInputs = with pkgs; [ nix home-manager git ];
   };
+
+  # For developing electron apps
+  # Wine is included only for cross-compiling Windows binary. Feel free to remove them if you don't need :)
+  electron = pkgs.mkShell {
+    nativeBuildInputs = pkgs:
+      (with pkgs; [ nodejs electron wine wine64 stdenv.cc.cc.lib ]);
+
+    LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib64:$LD_LIBRARY_PATH";
+    ELECTRON_OVERRIDE_DIST_PATH = "${pkgs.electron}/bin/";
+  };
 }
