2700ed7f7a26a44bd776126fc10938bc9082d93e
Bu Kun <65808665+pokon548@users.noreply.github.com>
2023-08-08 00:34:26 +0800

    Use lanzaboote v0.3.0 to avoid building toolchain

--- nixos/services/dae.nix
+++ nixos/services/dae.nix
@@ -1,44 +1,39 @@
 { config, pkgs, lib, ... }:
-
-with lib;
-
+let
+  cfg = config.services.dae;
+in
 {
   options = {
     services.dae = {
-      enable = options.mkEnableOption (mdDoc "the dae service");
+      enable = lib.options.mkEnableOption (lib.mdDoc "the dae service");
+      package = lib.mkPackageOptionMD pkgs "dae" { };
     };
   };
 
-  config = mkIf config.services.dae.enable {
-    environment.systemPackages = [ pkgs.dae ];
-
+  config = lib.mkIf config.services.dae.enable {
     networking.firewall.allowedTCPPorts = [ 12345 ];
     networking.firewall.allowedUDPPorts = [ 12345 ];
 
-    systemd.services.dae =
-      {
-        unitConfig = {
-          Description = "dae Service";
-          Documentation = "https://github.com/daeuniverse/dae";
-          After = [
-            "network.target"
-            "systemd-sysctl.service"
-          ];
-          Wants = [ "network.target" ];
-        };
-
-        serviceConfig = {
-          User = "root";
-          ExecStartPre = "${getExe pkgs.dae} validate -c /etc/dae/config.dae";
-          ExecStart = "${getExe pkgs.dae} run --disable-timestamp -c /etc/dae/config.dae";
-          ExecReload = "${getExe pkgs.dae} reload $MAINPID";
-          LimitNPROC = 512;
-          LimitNOFILE = 1048576;
-          Restart = "on-abnormal";
-          Type = "notify";
-        };
-
-        wantedBy = [ "multi-user.target" ];
+    systemd.services.dae = {
+      unitConfig = {
+        Description = "dae Service";
+        Documentation = "https://github.com/daeuniverse/dae";
+        After = [ "network.target" "systemd-sysctl.service" ];
+        Wants = [ "network.target" ];
       };
+
+      serviceConfig = {
+        User = "root";
+        ExecStartPre = "${lib.getExe cfg.package} validate -c /etc/dae/config.dae";
+        ExecStart = "${lib.getExe cfg.package} run --disable-timestamp -c /etc/dae/config.dae";
+        ExecReload = "${lib.getExe cfg.package} reload $MAINPID";
+        LimitNPROC = 512;
+        LimitNOFILE = 1048576;
+        Restart = "on-abnormal";
+        Type = "notify";
+      };
+
+      wantedBy = [ "multi-user.target" ];
+    };
   };
 }
--- nixos/surfacego.nix
+++ nixos/surfacego.nix
@@ -47,6 +47,6 @@
   };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
