de51c3b61d0eab25a3616acdb960b5fd92fcc52f
pokon548 <65808665+pokon548@users.noreply.github.com>
2023-08-13 14:54:03 +0800

    Use upstream dae module

--- nixos/software/workstation.nix
+++ nixos/software/workstation.nix
@@ -1,6 +1,4 @@
 { lib, config, pkgs, ... }: {
-  imports = [ ../services/dae.nix ];
-
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
