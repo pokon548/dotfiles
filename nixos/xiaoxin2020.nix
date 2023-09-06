eee2f249b994f465033177102656446c73baba7c
pokon548 <pokon548@hotmail.com>
2023-09-06 09:13:46 +0800

    Enable nvidia driver

--- nixos/xiaoxin2020.nix
+++ nixos/xiaoxin2020.nix
@@ -38,6 +38,33 @@
 
   hardware.enableRedistributableFirmware = true;
 
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = true;
+    open = false;
+    nvidiaSettings = true;
+    prime = {
+	    offload = {
+		    enable = true;
+		    enableOffloadCmd = true;
+	    };
+
+	    # Make sure to use the correct Bus ID values for your system!
+	    intelBusId = "PCI:0:2:0";
+	    nvidiaBusId = "PCI:1:0:0";
+	  };
+
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
   networking = {
     useDHCP = lib.mkDefault true;
     hostName = "xiaoxin";
