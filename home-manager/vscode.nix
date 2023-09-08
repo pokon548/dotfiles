6fe55c6772202e18f87df1ef06742ceea42221f8
Bu Kun <65808665+pokon548@users.noreply.github.com>
2023-09-08 09:31:01 +0800

    More vscode tuning

--- home-manager/vscode.nix
+++ home-manager/vscode.nix
@@ -4,7 +4,21 @@ let
     vscodevim.vim
     vscode-icons-team.vscode-icons
     ms-ceintl.vscode-language-pack-zh-hans
+    github.vscode-pull-request-github
     jnoortheen.nix-ide
+    gruntfuggly.todo-tree
+  ];
+  frontendDevExtensions = with pkgs.vscode-extensions; [
+    dbaeumer.vscode-eslint
+    esbenp.prettier-vscode
+    bradlc.vscode-tailwindcss
+  ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+    {
+      name = "vscode-postcss";
+      publisher = "vunguyentuan";
+      version = "2.0.2";
+      sha256 = "ttvCwxk3dMwva5LmVHq4p31INTa/T91qQISEU4gYNbg=";
+    }
   ];
 in
 
@@ -13,16 +27,21 @@ in
     enable = true;
     enableUpdateCheck = false;
     package = pkgs.vscodium;
-    extensions = commonExtensions;
+    extensions = commonExtensions ++ frontendDevExtensions;
     mutableExtensionsDir = false;
     userSettings = {
       "window.dialogStyle" = "custom";
       "window.titleBarStyle" = "custom";
       "workbench.iconTheme" = "vscode-icons";
+      "security.workspace.trust.enabled" = false;
+      "editor.fontFamily" = "'JetBrains Mono', 'Droid Sans Mono', 'monospace', monospace";
+      "window.zoomLevel" = 0.5;
     };
   };
 
   home.packages = with pkgs; [
     nixpkgs-fmt
+
+    jetbrains-mono
   ];
 }
