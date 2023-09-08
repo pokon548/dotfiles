fced1fd66661122c9b9954ca7a7649b3e05ba433
pokon548 <pokon548@hotmail.com>
2023-09-08 08:39:32 +0800

    Add more extensions

--- home-manager/vscode.nix
+++ home-manager/vscode.nix
@@ -1,17 +1,20 @@
-{ ... }:
+{ pkgs, ... }: let commonExtensions = with pkgs.vscode-extensions; [
+  vscodevim.vim
+  vscode-icons-team.vscode-icons
+  ms-ceintl.vscode-language-pack-zh-hans
+];
+in 
 
 {
-    programs.vscode = {
-      enable = true;
-      enableUpdateCheck = false;
-      package = pkgs.vscodium;
-      extensions = with pkgs.vscode-extensions; [
-        vscodevim.vim
-        ms-ceintl.vscode-language-pack-zh-hans
-      ];
-      userSettings = {
-        "window.dialogStyle" = "custom";
-        "window.titleBarStyle" = "custom";
-      };
+  programs.vscode = {
+    enable = true;
+    enableUpdateCheck = false;
+    package = pkgs.vscodium;
+    extensions = commonExtensions;
+    userSettings = {
+      "window.dialogStyle" = "custom";
+      "window.titleBarStyle" = "custom";
+      "workbench.iconTheme" = "vscode-icons";
     };
+  };
 }
