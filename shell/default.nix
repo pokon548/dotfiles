2f1f8cdbe8ddeb1ab71526656b612ca5bbd88d7a
pokon548 <pokon548@hotmail.com>
2023-09-08 16:31:20 +0800

    Make electron devshell useable

--- shell/default.nix
+++ shell/default.nix
@@ -11,19 +11,81 @@
 
   # For developing electron apps
   # Wine is included only for cross-compiling Windows binary. Feel free to remove them if you don't need :)
-  electron = pkgs.mkShell {
-    nativeBuildInputs = with pkgs; [
-      yarn
-      nodejs
-      electron
-      wine
-      wine64
-      stdenv.cc.cc.lib
-    ];
-
-    LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib64:$LD_LIBRARY_PATH";
-    ELECTRON_OVERRIDE_DIST_PATH = "${pkgs.electron}/bin/";
-  };
+  electron = (pkgs.buildFHSUserEnv
+    {
+      name = "electron-env";
+      targetPkgs = pkgs: (with pkgs;
+        [
+          nodejs
+          python3
+          libcxx
+          systemd
+          libpulseaudio
+          libdrm
+          mesa
+          stdenv.cc.cc
+          alsa-lib
+          atk
+          at-spi2-atk
+          at-spi2-core
+          cairo
+          cups
+          dbus
+          expat
+          fontconfig
+          freetype
+          gdk-pixbuf
+          glib
+          gtk3
+          libnotify
+          libuuid
+          nspr
+          nss
+          pango
+          systemd
+          libappindicator-gtk3
+          libdbusmenu
+          libxkbcommon
+          zlib
+          yarn
+          wine
+          wine64
+          ungoogled-chromium
+          cairo
+          pango
+          libjpeg
+          libpng
+          giflib
+          librsvg
+          pixman
+          pkg-config
+          glibc
+          gcc_debug
+          binutils
+          stdenv.cc.cc.lib
+          clang
+          llvmPackages.bintools
+          rustup
+          rust-analyzer
+        ]
+      ) ++ (with pkgs.xorg;
+        [
+          libXScrnSaver
+          libXrender
+          libXcursor
+          libXdamage
+          libXext
+          libXfixes
+          libXi
+          libXrandr
+          libX11
+          libXcomposite
+          libxshmfence
+          libXtst
+          libxcb
+        ]
+      );
+    }).env;
 
   learning = pkgs.mkShell {
     nativeBuildInputs = with pkgs; [
