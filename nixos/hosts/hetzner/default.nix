6e5e02b16f0ba5457865c9a3a15320b5ef633379
pokon548 <pokon548@hotmail.com>
2023-10-10 18:57:02 +0800

    Add hetzner key to secrets

--- nixos/hosts/hetzner/default.nix
+++ nixos/hosts/hetzner/default.nix
@@ -2,26 +2,33 @@
 
 {
   imports = [
-    "${inputs.nixpkgs}/nixos/modules/virtualisation/qemu-vm.nix"
+    "${inputs.nixpkgs}/nixos/modules/profiles/qemu-guest.nix"
   ] ++ (with inputs.nixos-hardware.nixosModules;
-  [
-    common-pc-ssd
-  ]);
+    [
+      common-pc-ssd
+    ]);
 
-  disko.devices = import ./disko-config.nix {
-    inherit lib;
-  };
-
-  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usbhid" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
-  boot.kernelModules = [ "kvm-intel" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "virtio_pci" "sd_mod" ];
+  boot.initrd.kernelModules = [ "virtio_gpu" ];
+  boot.kernelParams = [ "console=tty" ];
   boot.extraModulePackages = [ ];
 
-  boot.loader.grub = {
-    enable = true;
-    device = "/dev/vda";
-  };
+  boot.loader.systemd-boot.enable = true;
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  fileSystems."/" =
+    {
+      device = "/dev/sda2";
+      fsType = "btrfs";
+      options = [ "compress=zstd" ];
+    };
+
+  fileSystems."/boot" =
+    {
+      device = "/dev/sda1";
+      fsType = "vfat";
+    };
 
   networking = {
     useDHCP = lib.mkDefault true;
--- nixos/modules/synapse.nix
+++ nixos/modules/synapse.nix
@@ -1,5 +1,39 @@
-{ ... }: {
+{ pkgs, ... }: {
   services.matrix-synapse = {
     enable = true;
+    settings = {
+      server_name = "bukn.uk";
+
+      url_preview_enabled = true;
+      max_upload_size = "100M";
+      enable_registration = false;
+
+      listeners = [
+        {
+          bind_addresses = [ "127.0.0.1" "::1" ];
+          port = 8448;
+          type = "http";
+          tls = false;
+          x_forwarded = true;
+          resources = [{
+            compress = false;
+            names = [ "client" "federation" ];
+          }];
+        }
+      ];
+
+      registration_requires_token = true;
+    };
+  };
+
+  services.postgresql = {
+    enable = true;
+    initialScript = pkgs.writeText "synapse-init.sql" ''
+      CREATE ROLE "matrix-synapse" WITH LOGIN PASSWORD 'synapse';
+      CREATE DATABASE "matrix-synapse" WITH OWNER "matrix-synapse"
+        TEMPLATE template0
+        LC_COLLATE = "C"
+        LC_CTYPE = "C";
+    '';
   };
 }
