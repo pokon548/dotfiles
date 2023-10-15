ffec11f527e9b05b5b2a56e9cfbc5ec541e61729
pokon548 <pokon548@hotmail.com>
2023-10-15 21:16:28 +0800

    Add microbin

---
+++ nixos/modules/microbin.nix
@@ -0,0 +1,38 @@
+{ config, lib, pkgs, ... }:
+
+let
+  cfg = config.networking.microbin-server;
+in
+{
+  options = {
+    networking.microbin-server = with lib; {
+      enable = mkEnableOption (mdDoc "Microbin server");
+
+      stateDir = mkOption {
+        type = types.str;
+      };
+
+      environmentFile = mkOption {
+        type = types.path;
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    systemd.services.microbin = {
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" ];
+      description = "juicity daemon";
+      serviceConfig = {
+        Type = "simple";
+        User = "nobody";
+        RootDirectory= "/";
+        WorkingDirectory = "${cfg.stateDir}";
+
+        ExecStart = "${cfg.package}/bin/microbin"
+      };
+
+      serviceConfig.EnvironmentFile = cfg.environmentFile;
+    };
+  };
+}
