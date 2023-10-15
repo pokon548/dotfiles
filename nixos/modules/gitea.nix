f9975020add2f8236f255bef44e09d11ebe126da
Bu Kun <65808665+pokon548@users.noreply.github.com>
2023-10-15 19:07:58 +0800

    Try to fix gitea

--- nixos/modules/gitea.nix
+++ nixos/modules/gitea.nix
@@ -13,13 +13,12 @@ in
   config = lib.mkIf cfg.enable {
     services.gitea = {
       enable = true;
-      lfs.enable = true;
       settings = {
         service = {
           DISABLE_REGISTRATION = true;
         };
         server = {
-          DOMAIN = "gitea.bukn.uk";
+          ROOT_URL = "https://gitea.bukn.uk";
           START_SSH_SERVER = true;
           SSH_PORT = 22222;
           HTTP_PORT = 3001;
@@ -30,9 +29,12 @@ in
         other = {
           SHOW_FOOTER_VERSION = false;
         };
+        log = {
+          ROOT_PATH = "/mnt/external-storage/gitea/log";
+          LEVEL = "Info";
+        };
       };
       stateDir = "/mnt/external-storage/gitea";
-      after = [ "mnt-external\x2dstorage-gitea.mount" "var-lib-gitea.mount" ];
       database = {
         type = "postgres";
         user = "gitea";
@@ -40,14 +42,15 @@ in
       };
     };
 
+    systemd.services.gitea = {
+      after = [ "mnt-external\x2dstorage-gitea.mount" "var-lib-gitea.mount" ];
+      path = [ pkgs.bash ];
+    };
+
     networking.firewall =
       {
         allowedTCPPorts = [ 22222 ];
         allowedUDPPorts = [ 22222 ];
       };
-
-    systemd.services.gitea = {
-      path = [ pkgs.bash ];
-    };
   };
 }
