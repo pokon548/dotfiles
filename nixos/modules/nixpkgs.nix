06f56b01513be1147ca73fd87ab45250b49d7be8
pokon548 <pokon548@hotmail.com>
2023-10-21 17:51:20 +0800

    Use custom mutter

--- nixos/modules/nixpkgs.nix
+++ nixos/modules/nixpkgs.nix
@@ -1,16 +1,25 @@
-{ inputs, ... }: {
+{ inputs, pkgs, ... }: {
   nixpkgs = {
     overlays = [
       inputs.nur.overlay
       inputs.rust-overlay.overlays.default
       inputs.microvm.overlay
+
+      (self: super: {
+        gnome = super.gnome.overrideScope' (gself: gsuper: {
+          mutter = gsuper.mutter.overrideAttrs (old: {
+            patches = [ ./gnome-patch/mr1441.patch ./gnome-patch/mr3113.patch ];
+          });
+        });
+      })
     ];
 
-    config = {
-      allowUnfree = true;
-      permittedInsecurePackages = [
-        "openssl-1.1.1w"
-      ];
-    };
+    config =
+      {
+        allowUnfree = true;
+        permittedInsecurePackages = [
+          "openssl-1.1.1w"
+        ];
+      };
   };
 }
