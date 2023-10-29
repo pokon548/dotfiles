e71563e8a7bb0c6f5870ea02cde4f4a100e3fae4
pokon548 <pokon548@hotmail.com>
2023-10-29 19:16:57 +0800

    Revert "Fix wayland IME issue"
    This reverts commit 2d4dc459855fe5788c1adae01c2f1e403203ab84.

--- nixos/modules/fcitx5.nix
+++ nixos/modules/fcitx5.nix
@@ -1,10 +1,10 @@
 { config, pkgs, lib, ... }: {
   # Workaround for fcitx5 issue under wayland. See: https://github.com/NixOS/nixpkgs/issues/129442
-  environment.sessionVariables = lib.mkDefault {
+  environment.sessionVariables = {
     NIX_PROFILES =
       "${lib.concatStringsSep " " (lib.reverseList config.environment.profiles)}";
-    GTK_IM_MODULE = "wayland";
-    QT_IM_MODULE = "wayland";
+    GTK_IM_MODULE = "fcitx";
+    QT_IM_MODULE = "fcitx";
     XMODIFIERS = "@im=fcitx";
   };
 
