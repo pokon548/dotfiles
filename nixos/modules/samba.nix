0a846ac10dae5836ec621ba3be84157da1446254
Bu Kun <65808665+pokon548@users.noreply.github.com>
2023-12-04 19:37:04 +0800

    Continue remove pasta code for samba

--- nixos/modules/samba.nix
+++ nixos/modules/samba.nix
@@ -1,9 +1,19 @@
 { config, lib, pkgs, ... }:
 
-# TODO: Pasta code found. Any elegant ideas?
+# TODO: We should elimate mountFolder, as it is only created for maintain backward compability.
+# NEVER make mountFolder different from name in future service!
+
 let
   cfg = config.networking.samba;
-  services = [ "gitea" "seafile" "microbin" "wikijs" "send" ];
+  basePath = "/mnt/external-storage";
+  baseSMBPath = "u370687-sub";
+  services = [
+    { name = "gitea"; uid = 995; mountFolder = "gitea"; subAccountId = 2; }
+    { name = "seafile"; uid = 0; mountFolder = "seafile"; subAccountId = 3; } # TODO: Not safe to use root account
+    { name = "microbin"; uid = 65534; mountFolder = "pastebin"; subAccountId = 5; }
+    { name = "wikijs"; uid = 65534; mountFolder = "wiki-js"; subAccountId = 1; }
+    { name = "send"; uid = 65534; mountFolder = "send"; subAccountId = 8; }
+  ];
 in
 {
   options = {
@@ -22,9 +32,9 @@ in
         builtins.concatLists (
           map
             (x: [
-              { name = x + "-cifs-username"; value = { }; }
-              { name = x + "-cifs-password"; value = { }; }
-              { name = x + "-cifs-domain"; value = { }; }
+              { name = x.name + "-cifs-username"; value = { }; }
+              { name = x.name + "-cifs-password"; value = { }; }
+              { name = x.name + "-cifs-domain"; value = { }; }
             ])
             services));
 
@@ -33,12 +43,12 @@ in
           map
             (x: [
               {
-                name = x + "-smb-secrets";
+                name = x.name + "-smb-secrets";
                 value = {
                   content = ''
-                    username=${config.sops.placeholder."${x}-cifs-username"}
-                    domain=${config.sops.placeholder."${x}-cifs-domain"}
-                    password=${config.sops.placeholder."${x}-cifs-password"}
+                    username=${config.sops.placeholder."${x.name}-cifs-username"}
+                    domain=${config.sops.placeholder."${x.name}-cifs-domain"}
+                    password=${config.sops.placeholder."${x.name}-cifs-password"}
                   '';
                 };
               }
@@ -46,64 +56,26 @@ in
             services));
     };
 
-    fileSystems."/mnt/external-storage/wiki-js" = {
-      device = "//u370687-sub1.your-storagebox.de/u370687-sub1";
-      fsType = "cifs";
-      options =
-        let
-          # this line prevents hanging on network split
-          automount_opts = "_netdev,x-systemd.automount,nofail,x-systemd.device-timeout=10ms,mfsymlinks,uid=65534,gid=65534";
+    fileSystems = builtins.listToAttrs
+      (
+        builtins.concatLists (
+          map
+            (x: [
+              {
+                name = basePath + "/" + x.mountFolder;
+                value = {
+                  device = "//" + baseSMBPath + builtins.toString (x.subAccountId) + ".your-storagebox.de/" + baseSMBPath + builtins.toString (x.subAccountId);
+                  fsType = "cifs";
+                  options =
+                    let
+                      # this line prevents hanging on network split
+                      automount_opts = "_netdev,x-systemd.automount,nofail,x-systemd.device-timeout=10ms,mfsymlinks,uid=${builtins.toString (x.uid)},gid=${builtins.toString (x.uid)}";
 
-        in
-        [ "${automount_opts},credentials=${config.sops.templates."wikijs-smb-secrets".path}" ];
-    };
-
-    fileSystems."/mnt/external-storage/gitea" = {
-      device = "//u370687-sub2.your-storagebox.de/u370687-sub2";
-      fsType = "cifs";
-      options =
-        let
-          # this line prevents hanging on network split
-          automount_opts = "_netdev,x-systemd.automount,nofail,x-systemd.device-timeout=10ms,mfsymlinks,uid=995,gid=995";
-
-        in
-        [ "${automount_opts},credentials=${config.sops.templates."gitea-smb-secrets".path}" ];
-    };
-
-    fileSystems."/mnt/external-storage/seafile" = {
-      device = "//u370687-sub3.your-storagebox.de/u370687-sub3";
-      fsType = "cifs";
-      options =
-        let
-          # this line prevents hanging on network split
-          automount_opts = "_netdev,x-systemd.automount,hard,nofail,x-systemd.device-timeout=10ms,mfsymlinks,uid=0,gid=0";
-
-        in
-        [ "${automount_opts},credentials=${config.sops.templates."seafile-smb-secrets".path}" ];
-    };
-
-    fileSystems."/mnt/external-storage/pastebin" = {
-      device = "//u370687-sub5.your-storagebox.de/u370687-sub5";
-      fsType = "cifs";
-      options =
-        let
-          # this line prevents hanging on network split
-          automount_opts = "_netdev,x-systemd.automount,nofail,x-systemd.device-timeout=10ms,mfsymlinks,uid=65534,gid=65534";
-
-        in
-        [ "${automount_opts},credentials=${config.sops.templates."microbin-smb-secrets".path}" ];
-    };
-
-    fileSystems."/mnt/external-storage/send" = {
-      device = "//u370687-sub6.your-storagebox.de/u370687-sub8";
-      fsType = "cifs";
-      options =
-        let
-          # this line prevents hanging on network split
-          automount_opts = "_netdev,x-systemd.automount,nofail,x-systemd.device-timeout=10ms,mfsymlinks,uid=65534,gid=65534";
-
-        in
-        [ "${automount_opts},credentials=${config.sops.templates."send-smb-secrets".path}" ];
-    };
+                    in
+                    [ "${automount_opts},credentials=${config.sops.templates."${x.name}-smb-secrets".path}" ];
+                };
+              }
+            ])
+            services));
   };
 }
