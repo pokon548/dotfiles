41dd6ed77542f81e0531884d52bed5eb5bca68f7
Bu Kun <65808665+pokon548@users.noreply.github.com>
2023-12-04 20:10:35 +0800

    Remove pasta code for caddy

--- nixos/modules/caddy/hetzner.nix
+++ nixos/modules/caddy/hetzner.nix
@@ -1,4 +1,17 @@
-{ config, ... }: {
+{ config, ... }:
+let
+  forwardHosts = [
+    { name = "chat"; port = 8448; }
+    { name = "gitea"; port = 3001; }
+    { name = "pb"; port = 36721; }
+    { name = "telemetry"; port = 3000; }
+    { name = "cloud"; port = 8088; }
+    { name = "webdav"; port = 46732; }
+    { name = "send"; port = 1443; }
+    { name = "a"; port = 47192; }
+  ];
+in
+{
   networking.firewall.allowedTCPPorts = [
     80
     443
@@ -6,113 +19,66 @@
 
   services.caddy = {
     enable = true;
-    virtualHosts."www.bukn.uk".extraConfig = ''
-      tls me@www.bukn.uk
-      header / Strict-Transport-Security "max-age=63072000;includeSubDomains;preload"
 
-      redir https://bukn.uk{uri}
-    '';
+    virtualHosts = (builtins.listToAttrs
+      (
+        builtins.concatLists (
+          map
+            (x: [
+              {
+                name = "${x.name}.bukn.uk";
+                value = {
+                  extraConfig = ''
+                    tls me@${x.name}.bukn.uk
+                    header / Strict-Transport-Security "max-age=63072000;includeSubDomains;preload"
 
-    virtualHosts."bukn.uk".extraConfig = ''
-      tls me@bukn.uk
-      header / Strict-Transport-Security "max-age=63072000;includeSubDomains;preload"
+                    reverse_proxy localhost:${builtins.toString (x.port)}
+                  '';
+                };
+              }
+            ])
+            forwardHosts)) //
+    {
+      "bukn.uk".extraConfig = ''
+              tls me@bukn.uk
+              header / Strict-Transport-Security "max-age=63072000;includeSubDomains;preload"
 
-      header /.well-known/matrix/* Content-Type application/json
-      header /.well-known/matrix/* Access-Control-Allow-Origin *
-      respond /.well-known/matrix/server `{"m.server": "chat.bukn.uk:443"}`
-      respond /.well-known/matrix/client `{"m.homeserver":{"base_url":"https://chat.bukn.uk"}}`
+              header /.well-known/matrix/* Content-Type application/json
+              header /.well-known/matrix/* Access-Control-Allow-Origin *
+              respond /.well-known/matrix/server `{"m.server": "chat.bukn.uk:443"}`
+              respond /.well-known/matrix/client `{"m.homeserver":{"base_url":"https://chat.bukn.uk"}}`
 
-      handle_path /_matrix/* {
-        rewrite * /_matrix{uri}
-	      reverse_proxy localhost:8448
-      }
+              handle_path /_matrix/* {
+                rewrite * /_matrix{uri}
+        	      reverse_proxy localhost:8448
+              }
 
-      handle_path /_synapse/* {
-        rewrite * /_synapse{uri}
-	      reverse_proxy localhost:8448
-      }
-    '';
+              handle_path /_synapse/* {
+                rewrite * /_synapse{uri}
+        	      reverse_proxy localhost:8448
+              }
+      '';
+    } //
+    {
+      "wiki.bukn.uk".extraConfig = ''
+        tls me@wiki.bukn.uk
+        header / Strict-Transport-Security "max-age=63072000;includeSubDomains;preload"
 
-    virtualHosts."chat.bukn.uk".extraConfig = ''
-      tls me@chat.bukn.uk
-      header / Strict-Transport-Security "max-age=63072000;includeSubDomains;preload"
+        reverse_proxy /sitemap.xml localhost:3012
+        reverse_proxy localhost:46178
+      '';
+    } //
+    {
+      "authentik.bukn.uk".extraConfig = ''
+        tls me@authentik.bukn.uk
+        header / Strict-Transport-Security "max-age=63072000;includeSubDomains;preload"
 
-      reverse_proxy localhost:8448
-    '';
-
-    virtualHosts."gitea.bukn.uk".extraConfig = ''
-      tls me@gitea.bukn.uk
-      header / Strict-Transport-Security "max-age=63072000;includeSubDomains;preload"
-
-      reverse_proxy localhost:3001
-    '';
-
-    virtualHosts."pb.bukn.uk".extraConfig = ''
-      tls me@pb.bukn.uk
-      header / Strict-Transport-Security "max-age=63072000;includeSubDomains;preload"
-
-      reverse_proxy localhost:36721
-    '';
-
-    virtualHosts."wiki.bukn.uk".extraConfig = ''
-      tls me@wiki.bukn.uk
-      header / Strict-Transport-Security "max-age=63072000;includeSubDomains;preload"
-
-      reverse_proxy /sitemap.xml localhost:3012
-      reverse_proxy localhost:46178
-    '';
-
-    virtualHosts."telemetry.bukn.uk".extraConfig = ''
-      tls me@telemetry.bukn.uk
-      header / Strict-Transport-Security "max-age=63072000;includeSubDomains;preload"
-
-      reverse_proxy localhost:3000
-    '';
-
-    virtualHosts."authentik.bukn.uk".extraConfig = ''
-      tls me@authentik.bukn.uk
-      header / Strict-Transport-Security "max-age=63072000;includeSubDomains;preload"
-
-      reverse_proxy https://localhost:9443 {
-        transport http {
-			    tls_insecure_skip_verify
-		    }
-      }
-    '';
-
-    virtualHosts."filestash.bukn.uk".extraConfig = ''
-      tls me@authentik.bukn.uk
-      header / Strict-Transport-Security "max-age=63072000;includeSubDomains;preload"
-
-      reverse_proxy http://localhost:8334
-    '';
-
-    virtualHosts."cloud.bukn.uk".extraConfig = ''
-      tls me@cloud.bukn.uk
-      header / Strict-Transport-Security "max-age=63072000;includeSubDomains;preload"
-
-      reverse_proxy localhost:8088
-    '';
-
-    virtualHosts."webdav.bukn.uk".extraConfig = ''
-      tls me@webdav.bukn.uk
-      header / Strict-Transport-Security "max-age=63072000;includeSubDomains;preload"
-
-      reverse_proxy localhost:46732
-    '';
-
-    virtualHosts."send.bukn.uk".extraConfig = ''
-      tls me@send.bukn.uk
-      header / Strict-Transport-Security "max-age=63072000;includeSubDomains;preload"
-
-      reverse_proxy localhost:1443
-    '';
-
-    virtualHosts."a.bukn.uk".extraConfig = ''
-      tls me@a.bukn.uk
-      header / Strict-Transport-Security "max-age=63072000;includeSubDomains;preload"
-
-      reverse_proxy localhost:47192
-    '';
+        reverse_proxy https://localhost:9443 {
+          transport http {
+        		tls_insecure_skip_verify
+        	}
+        }
+      '';
+    });
   };
 }
