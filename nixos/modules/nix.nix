2b06918a581eb5a75732adcba1b118b910fec68c
Bu Kun <65808665+pokon548@users.noreply.github.com>
2023-12-16 11:57:40 +0800

    Use sjtu mirror

--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -22,7 +22,7 @@
       experimental-features = "nix-command flakes";
       auto-optimise-store = true;
       substituters = [
-        "https://mirrors.sjtug.sjtu.edu.cn/nix-channels/store"
+        "https://mirror.sjtu.edu.cn/nix-channels/store"
         "https://nix-community.cachix.org"
         "https://microvm.cachix.org"
         "https://pokon548.cachix.org"
